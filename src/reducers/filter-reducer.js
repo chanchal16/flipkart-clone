ab7390a4c21ed7fd98a9a41295a7de47e8450015
chanchal16 <rajputchanchal17@gmail.com>
2022-07-07 00:53:47 +0530

    Update - add clear filters

--- src/reducers/filter-reducer.js
+++ src/reducers/filter-reducer.js
@@ -1,39 +1,41 @@
 const filterReducer = (state, action) => {
     switch(action.type){
-        case 'GET_ALL_PRODUCTS':
-        return {...state,products:action.payload};
       case "LOW_TO_HIGH":
         return {
           ...state,
           sortBy:action.type
         }
+        
       case "HIGH_TO_LOW":
         return {
           ...state,
           sortBy:action.type
-        }    
+        }  
+
       case 'BRANDS':
         return state.brands.includes(action.payload)? 
         { ...state,brands:[...state.brands.filter(item=> item !== action.payload)]}
            : {...state, brands:[...state.brands,action.payload]}
+
       case "SIZES":
         return {
-          ...state,
-          sizes:action.payload
-        }
+            ...state,
+            sizes: { ...state.sizes, [action.payload]: !state.sizes[action.payload] },
+        };
+
       case "GENDER":
-        return {
-          ...state,
-          range:action.payload
-        }
+        return state.gender.includes(action.payload)? 
+        { ...state,gender:[state.gender.filter(item=> item !== action.payload)]}
+           : {...state, gender:[...state.gender,action.payload]}
+
       case 'CLEAR':
         return {
           ...state,
           sortBy:'',
           products:[],
-          brands:['Tokyo Talkies','Harpa','HighLander','Raymond'],
-          sizes:['S','M','L','XL'] ,
-          gender:['male','female']  
+          brands:[],
+          sizes:{  S: false,M: false,L: false, XL: false,} ,
+          gender:[]  
         }
       default:
         return state;
