7e94fce21f5ca58ad5ad4b2443acc85b9b4764fb
chanchal16 <rajputchanchal17@gmail.com>
2022-07-07 00:52:26 +0530

    style - add margin to brand name

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 
 export const ProductCard = ({product}) => {
   return (
-    <div className="bg-gray-100 w-52 flex flex-col rounded border border-gray-3 shadow-md-gray-1 mb-4 p-2">
+    <div className="bg-white w-52 flex flex-col rounded border border-gray-3 shadow-md-gray-1 mb-4 p-2">
         <h3 className="text-gray-5 font-medium mb-2">{product.name}</h3>
         <img src={product.imgsrc} alt="product" className="w-24 h-32" style={{margin: "auto"}} />
 
         <div className="mt-2 mb-2 flex justify-between items-center">
             <h4 className="text-gray-5 mt-2">$ {product.price}</h4>
-            <small>Brand: {product.brand}</small>
+            <small className='my-auto'>Brand: {product.brand}</small>
         </div>
         <div className="mt-2 mb-2 flex justify-between items-center">
             <small className="text-gray-5">Size: {product.size}</small>
-            {/* <small className="textGray5">Ideal for: {product.ideal}</small> */}
         </div>
     </div>
   )
