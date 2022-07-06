b55aa1278dafb031e4674304eece752ce675555c
chanchal16 <rajputchanchal17@gmail.com>
2022-07-07 00:53:23 +0530

    Update - remove commented code

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,40 +8,18 @@ export const Products = () => {
     const {filterState,filterDispatch} = useFilter();
     const{gender,brands,sizes,sortBy} = filterState;
 
-    // useEffect(() => {
-    //       filterDispatch({type:'GET_ALL_PRODUCTS',payload:products})
-        
-    // }, [])
-
     const sortedProducts = getSortedProducts(products,sortBy);
     const sortedByGender = getFilteredByGender(sortedProducts,gender);
-    const sortedByBrand = getFliteredByBrands(sortedProducts,brands);
+    const sortedByBrand = getFliteredByBrands(sortedByGender,brands);
     const filteredProducts = getFilteredBySizes(sortedByBrand,sizes)
-    console.log('filtered',sortedByBrand)
+    console.log('filtered',sortedByGender)
   return (
     <div className="product-list container-mid mt-8 mb-8 grid place-items-center place-self-center  md:grid-cols-4">
         {
-            sortedByBrand?.map(product=>(
+            filteredProducts?.map(product=>(
                 <ProductCard key={product.id} product={product}/>
             ))
         }
-            {/* {
-                filteredProducts?.length > 0 && Object.keys(appliedFilters).length > 0 ?
-                filteredProducts.map(product => {
-                    return(
-                        <ProductCard key={product._id} product={product} />
-                    )
-                })
-                :
-                filteredProducts?.length < 1 && Object.keys(appliedFilters).length > 0 ?
-                <div className="">No matches found..</div>
-                :
-                productsList.map(product => {
-                    return(
-                        <ProductCard key={product._id} product={product} />
-                    )
-                })
-            } */}
         </div>
   )
 }
